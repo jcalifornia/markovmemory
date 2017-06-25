2c168792064f23ea612a05cfbda9a3afc62322f5
Josh Chang <joshchang@ucla.edu>
2017-06-25 18:25:49 -0400

    hybrid

--- numerics/simulation.py
+++ numerics/simulation.py
@@ -101,6 +101,106 @@ def infer_model(trajectories,states,alpha,q):
     return probs, N
 
 
+def hybrid_info(trajectories, alpha):
+    cumulative_lppd = 0
+    cumulative_kWAIC2 = 0
+    cumulative_loo = 0
+    thiscounts = []
+    globalcounts = np.zeros((2, 2))
+    for trajectory in trajectories:
+        miss_pos = [m.end() for m in re.finditer("\-", trajectory)]
+        if len(trajectory) in miss_pos: miss_pos.remove(len(trajectory))
+        nextshot = [trajectory[j] for j in miss_pos]
+        nextmakes = nextshot.count("+")
+        nexttotal = len(nextshot)
+        othermakes = trajectory.count("+") - nextmakes
+        othertotal = len(trajectory) - nexttotal
+        thiscounts.append(np.array([[nextmakes, nexttotal - nextmakes],
+                                    [othermakes, othertotal - othermakes]]))
+        globalcounts += thiscounts[-1]
+        # shots in these positions are accounted for. All other shots are independent
+
+    global_row_sums = np.sum(globalcounts + alpha,
+                             axis=1)  # total number of times a history is seen globall
+    for thiscount in thiscounts:
+
+        # only take nonzero rows of thiscount
+        rowsums = np.sum(thiscount, axis=1)
+        if rowsums[0] == 0 and rowsums[1] == 0:
+            continue
+        elif rowsums[0] == 0:
+            thiscountmatrix = thiscount[1, :].reshape((1, 2))
+            globalcountmatrix = globalcounts[1, :].reshape((1, 2))
+        elif rowsums[1] == 0:
+            thiscountmatrix = thiscount[0, :].reshape((1, 2))
+            globalcountmatrix = globalcounts[0, :].reshape((1, 2))
+        else:
+            thiscountmatrix = thiscount
+            globalcountmatrix = globalcounts
+
+        globalcountmatrix_loo = globalcountmatrix - thiscountmatrix
+
+        this_log_gammas = gammaln(thiscountmatrix + globalcountmatrix + alpha)
+        global_log_gammas = gammaln(globalcountmatrix + alpha)
+        global_log_gammas_loo = gammaln(globalcountmatrix_loo + alpha)
+
+        # do computations on the individual trajectory level
+        try:
+            this_row_sums = np.sum(thiscountmatrix + globalcountmatrix + alpha,
+                                   axis=1)  # total number of times a history is seen locally
+        except BaseException as e:
+            pass
+
+        global_row_sums = np.sum(globalcountmatrix + alpha,
+                                 axis=1)  # total number of times a history is seen globally
+        global_row_sums_loo = np.sum(globalcountmatrix_loo + alpha, axis=1)
+
+        """
+        The lppd can be computed exactly
+        \begin{align}
+        \sum_{\bx}\log\mathbb{E}_{\bp_\bx\vert\bN_\bx}  \left[     \Pr\left(\mathbf{N}^{(j)}_{\bx} \mid \bp_\bx \right) \right]  \nonumber\\
+        &= \sum_j\sum_{\mathbf{x}} \log \mathbb{E}_{\bp_\bx\vert\bN_\bx} \left(% \frac{N_{\mathbf{x}}!}{\mathbf{N}_{\mathbf{x}}!}
+         \prod_{m=1}^{M} p_{\bx,m}^{N^{(j)}_{\bx,m}} \right) \nonumber\\
+        &=\sum_j \sum_{\bx}  \log\left(  \frac{B(\bN_\bx +\bN_{\bx}^{(j)} +\balpha)}{B(\bN_\bx +\balpha)} \right).
+        \end{align}
+        """
+
+        # this trajectory's contribution to the lppd
+        partial_lppd = np.sum(np.sum(this_log_gammas, axis=1) - gammaln(this_row_sums) - \
+                              np.sum(global_log_gammas, axis=1) + gammaln(global_row_sums))
+        partial_loo = np.sum(np.sum(global_log_gammas, axis=1) - gammaln(global_row_sums) - \
+                             np.sum(global_log_gammas_loo, axis=1) + gammaln(global_row_sums_loo))
+
+        partial_kWAIC2 = np.sum(thiscountmatrix ** 2 * polygamma(1, alpha + globalcountmatrix)) - \
+                         np.sum(np.sum(thiscountmatrix, axis=1) ** 2 * polygamma(1,
+                                                                                 np.sum(globalcountmatrix + alpha,
+                                                                                        axis=1)))
+
+        cumulative_lppd += partial_lppd
+        cumulative_loo += partial_loo
+        cumulative_kWAIC2 += partial_kWAIC2
+
+    totalrowsums = np.sum(globalcounts, axis=1)
+    logp_MLE = np.log(globalcounts / totalrowsums[:, None])
+    logp_MLE[np.isneginf(logp_MLE)] = 0
+
+    Nlogp_MLE = np.sum(globalcounts * logp_MLE)
+    Nlogp2 = np.sum(
+        globalcounts * log((globalcounts + alpha) / np.sum(globalcounts + alpha, axis=1)[:, None]))
+
+    expected_log_posterior = np.sum(globalcounts * (
+        polygamma(0, alpha + globalcounts) - polygamma(0, np.sum(globalcounts + alpha, axis=1))[:, None]))
+
+    kAIC = globalcounts.shape[0] * (globalcounts.shape[1] - 1)
+    kWAIC1 = 2 * cumulative_lppd - 2 * expected_log_posterior
+    kWAIC2 = cumulative_kWAIC2
+    WAIC1 = -2 * (cumulative_lppd - kWAIC1)
+    WAIC2 = -2 * (cumulative_lppd - kWAIC2)
+    AIC = -2 * Nlogp_MLE + 2 * kAIC
+    LOO = -2 * (cumulative_loo)
+
+    return {"AIC": AIC, "WAIC1": WAIC1, "WAIC2": WAIC2, "LOO": LOO}
+
 def evaluate_models(trajectories, states, alpha=1, qbounds=(1, 8)):
     WAIC1 = {}
     WAIC2 = {}
@@ -228,217 +328,6 @@ def evaluate_models(trajectories, states, alpha=1, qbounds=(1, 8)):
             cumulative_loo += partial_loo
             cumulative_kWAIC2 += partial_kWAIC2
             cumulative_cv2 += partial_cv2
-
-        # AIC and DIC don't need individual trajectories
-        totalcountmatrix = dict_to_mat(N[q])
-
-        totalrowsums = np.sum(totalcountmatrix, axis=1)
-        logp_MLE = np.log(totalcountmatrix / totalrowsums[:, None])
-        logp_MLE[np.isneginf(logp_MLE)] = 0
-
-        Nlogp_MLE = np.sum(totalcountmatrix * logp_MLE)
-        Nlogp2 = np.sum(
-            totalcountmatrix * log((totalcountmatrix + alpha) / np.sum(totalcountmatrix + alpha, axis=1)[:, None]))
-
-        expected_log_posterior = np.sum(totalcountmatrix * (
-        polygamma(0, alpha + totalcountmatrix) - polygamma(0, np.sum(totalcountmatrix + alpha, axis=1))[:, None]))
-        kDIC1[q] = 2 * np.sum(totalcountmatrix * (
-        np.log(totalcountmatrix + alpha) - np.log(np.sum(totalcountmatrix + alpha, axis=1))[:,
-                                           None])) - 2 * expected_log_posterior
-
-        kDIC2[q] = 2 * np.sum(totalcountmatrix ** 2 * polygamma(1, alpha + totalcountmatrix)) \
-                   - 2 * np.sum(totalrowsums ** 2 * polygamma(1, totalrowsums + totalcountmatrix.shape[1] * alpha))
-
-        kWAIC1[q] = 2 * cumulative_lppd - 2 * expected_log_posterior
-        kWAIC2[q] = cumulative_kWAIC2
-        DIC1[q] = -2 * Nlogp2 + 2 * kDIC1[q]
-        DIC2[q] = -2 * Nlogp2 + 2 * kDIC2[q]
-        WAIC1[q] = -2 * (cumulative_lppd - kWAIC1[q])
-        WAIC2[q] = -2 * (cumulative_lppd - kWAIC2[q])
-        kAIC = totalcountmatrix.shape[0] * (totalcountmatrix.shape[1] - 1)
-        AIC[q] = -2 * Nlogp_MLE + 2 * kAIC
-        LPPD[q] = -2 * (cumulative_lppd)
-        LOO[q] = -2 * (cumulative_loo)
-
-        log2gammas = gammaln(2 * totalcountmatrix + alpha)
-        log1gammas = gammaln(totalcountmatrix + alpha)
-
-        LPD[q] = -2 * (np.sum(np.sum(log2gammas, axis=1) - gammaln(2 * totalrowsums + alpha) - \
-                              np.sum(log1gammas, axis=1) + gammaln(totalrowsums + alpha)))
-
-        LPPDCV2[q] = -2 * (cumulative_cv2)
-
-    return {"WAIC1": WAIC1, "WAIC2": WAIC2, "LOO": LOO, "LPPDCV2": LPPDCV2,
-            "DIC1": DIC1, "DIC2": DIC2, "AIC": AIC, "LPPD": LPPD, "LPD": LPD,
-            "kWAIC1": kWAIC1, "kWAIC2": kWAIC2, "kDIC1": kDIC1, "kDIC2": kDIC2}
-
-def fit_jagged_model(trajectories, states, prestates = [''], alpha = 1):
-    """
-    Fit jagged model with given prestates. Any transition not found in prestates
-    is independent. Assume that prestates do not have more than one leading 0
-    :param trajectories:
-    :param states:
-    :param prestates:
-    :param alpha:
-    :return:
-    """
-
-    N = defaultdict(partial(np.zeros,len(states)))
-
-    max_q = max(1,max(len(p) for p in prestates))
-    print(max_q)
-
-    start_prestates = []
-    for prestate in prestates:
-        if prestate[0] == '0':
-            start_prestates.append(prestate)
-            prestates.remove(prestate)
-
-    prestates0 = sorted(prestates, key = lambda x: len(x),reverse = True)
-
-
-    for trajectory in trajectories:
-        pass
-
-    probs = defaultdict(partial(np.zeros, len(states)))
-    for key, val in N.items():
-        if sum(N[key]) > 0:
-            probs[key] = (N[key] + alpha) / (np.sum(N[key]) + alpha * len(states))
-    return probs, N
-
-def evaluate_jagged_model(trajectories, prestates, states, alpha):
-    """
-    Compute model criteria for given jagged model
-    :param trajectories:
-    :param model:
-    :param alpha:
-    :return:
-    """
-    WAIC1 = None
-    WAIC2 = None
-    DIC1 = None
-    DIC2 = None
-    AIC = None
-
-    kWAIC1 = None
-    kWAIC2 = None
-    kDIC1 = None
-    kDIC2 = None
-
-    LPPD = None
-    LPD = None
-    LOO = None
-    LPPDCV2 = None
-
-    # print(f"Fitting models of {q} states of hysteresis")
-    N = defaultdict(partial(np.zeros, len(states)))
-    N2 = defaultdict(partial(np.zeros, len(states)))  # sums for the first half of trajectories
-
-    cumulative_lppd = 0
-    cumulative_kWAIC2 = 0
-    cumulative_loo = 0
-    cumulative_cv2 = 0
-
-    # collect the prestates, finding values of q
-
-    J = len(trajectories)
-    K = len(prestates)
-
-    for j, trajectory in enumerate(trajectories):
-        trajectory = "".join(['0'] * (q)) + trajectory
-        for l in range(q, len(trajectory)):
-            if q == 0:
-                x = ''
-            else:
-                x = trajectory[(l - q):(l)]
-                # if x[-1] == '0' and len(x)>1: continue
-            m = states.index(trajectory[l])
-            N[x][m] += 1
-            if j < J / 2:
-                N2[x][m] += 1
-
-    for j, trajectory in enumerate(trajectories):
-        trajectory = "".join(['0'] * (q)) + trajectory
-        Nj = defaultdict(partial(np.zeros, len(states)))
-        for l in range(q, len(trajectory)):
-            if q == 0:
-                x = ''
-            else:
-                x = trajectory[(l - q):(l)]
-                # if x[-1] == '0' and len(x)>1: continue
-            Nj[x][states.index(trajectory[l])] += 1
-
-        thiscountmatrix = []
-        globalcountmatrix = []
-        halfglobalcountmatrix = []
-        for key, val in Nj.items():
-            thiscountmatrix += [val]  # counts for this trajectory
-            globalcountmatrix += [N[q][key]]  # total counts across all trajectories
-            halfglobalcountmatrix += [N2[q][key]]
-
-        # recast to numpy
-        thiscountmatrix = np.array(thiscountmatrix, dtype=np.int64)  # N^{(j)}_\bx
-        globalcountmatrix = np.array(globalcountmatrix, dtype=np.int64)  # N_\bx
-
-        if j >= J / 2:  # want the other half
-            # want the first half if our trajectory is from the second half
-            halfglobalcountmatrix = np.array(halfglobalcountmatrix, dtype=np.int64)
-        else:
-            # want the second half if our trajectory is from the first half
-            halfglobalcountmatrix = globalcountmatrix - np.array(halfglobalcountmatrix, dtype=np.int64)
-
-        globalcountmatrix_loo = globalcountmatrix - thiscountmatrix
-
-        this_log_gammas = gammaln(thiscountmatrix + globalcountmatrix + alpha)
-        global_log_gammas = gammaln(globalcountmatrix + alpha)
-        global_log_gammas_loo = gammaln(globalcountmatrix_loo + alpha)
-
-        # do computations on the individual trajectory level
-        try:
-            this_row_sums = np.sum(thiscountmatrix + globalcountmatrix + alpha,
-                                   axis=1)  # total number of times a history is seen locally
-        except BaseException as e:
-            pass
-
-        global_row_sums = np.sum(globalcountmatrix + alpha,
-                                 axis=1)  # total number of times a history is seen globally
-        global_row_sums_loo = np.sum(globalcountmatrix_loo + alpha, axis=1)
-
-        half_global_row_sums = np.sum(halfglobalcountmatrix, axis=1)
-        half_global_log_gammas = gammaln(halfglobalcountmatrix + alpha)
-
-        half_global_log_gammas_cv2 = gammaln(halfglobalcountmatrix + thiscountmatrix + alpha)
-        half_global_row_sums_cv2 = np.sum(halfglobalcountmatrix + thiscountmatrix + alpha, axis=1)
-
-        """
-        The lppd can be computed exactly
-        \begin{align}
-        \sum_{\bx}\log\mathbb{E}_{\bp_\bx\vert\bN_\bx}  \left[     \Pr\left(\mathbf{N}^{(j)}_{\bx} \mid \bp_\bx \right) \right]  \nonumber\\
-        &= \sum_j\sum_{\mathbf{x}} \log \mathbb{E}_{\bp_\bx\vert\bN_\bx} \left(% \frac{N_{\mathbf{x}}!}{\mathbf{N}_{\mathbf{x}}!}
-         \prod_{m=1}^{M} p_{\bx,m}^{N^{(j)}_{\bx,m}} \right) \nonumber\\
-        &=\sum_j \sum_{\bx}  \log\left(  \frac{B(\bN_\bx +\bN_{\bx}^{(j)} +\balpha)}{B(\bN_\bx +\balpha)} \right).
-        \end{align}
-        """
-
-        # this trajectory's contribution to the lppd
-        partial_lppd = np.sum(np.sum(this_log_gammas, axis=1) - gammaln(this_row_sums) - \
-                              np.sum(global_log_gammas, axis=1) + gammaln(global_row_sums))
-        partial_loo = np.sum(np.sum(global_log_gammas, axis=1) - gammaln(global_row_sums) - \
-                             np.sum(global_log_gammas_loo, axis=1) + gammaln(global_row_sums_loo))
-
-        partial_cv2 = np.sum(np.sum(half_global_log_gammas_cv2, axis=1) - gammaln(half_global_row_sums_cv2) - \
-                             np.sum(half_global_log_gammas, axis=1) + gammaln(
-            half_global_row_sums + this_log_gammas.shape[1] * alpha))
-
-        partial_kWAIC2 = np.sum(thiscountmatrix ** 2 * polygamma(1, alpha + globalcountmatrix)) - \
-                         np.sum(np.sum(thiscountmatrix, axis=1) ** 2 * polygamma(1,
-                                                                                 np.sum(globalcountmatrix + alpha,
-                                                                                        axis=1)))
-
-        cumulative_lppd += partial_lppd
-        cumulative_loo += partial_loo
-        cumulative_kWAIC2 += partial_kWAIC2
-        cumulative_cv2 += partial_cv2
 
         # AIC and DIC don't need individual trajectories
         totalcountmatrix = dict_to_mat(N[q])
